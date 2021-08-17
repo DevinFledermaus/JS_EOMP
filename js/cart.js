bde46d864e843514fdf06032a7e2d225e4631126
Koumori97 <d.e.fledermaus86@gmail.com>
2021-08-17 16:15:41 +0200

    updated files

--- js/cart.js
+++ js/cart.js
@@ -1,96 +1,3 @@
-// let cart = [
-//   {
-//     class: "card1",
-//     title: "Hugo Boss",
-//     description: "Makes stink stink go bye bye.",
-//     techStack: "Personal Hygiene",
-//     price: "R694.20",
-//     image: "./images/cologne.jpg",
-//     image_alt: "Cologne",
-//   },
-//   {
-//     class: "card2",
-//     title: "Taco shells",
-//     description: "Get these taco shells, but a little dry without the mix.",
-//     techStack: "Food",
-//     price: "R54.20",
-//     image: "./images/tacos.jpeg",
-//     image_alt: "Taco Shells",
-//   },
-//   {
-//     class: "card3",
-//     title: "Milk",
-//     description: "Coffee is not the same without this guy.",
-//     techStack: "Food",
-//     price: "R16.90",
-//     image: "./images/milk.jpeg",
-//     image_alt: "Milk",
-//   },
-// ];
-
-// function createCard(card) {
-//   let createdCard = `<div class="card ${card.class}" techStack="${card.techStack}">
-//             <div class="card-image">
-//             <img src="${card.image}" alt="${card.image_alt}" />
-//             </div>
-//             <div class="card-content">
-//             <h2>${card.title}</h2>
-//             <p>${card.description}</p>
-//             <h4>${card.price}</h4>
-//             </div>
-//           </div>
-//         `;
-//   return createdCard;
-// }
-
-// function renderCards() {
-//   let productContainer = document.querySelector(".products");
-//   for (product of cart) {
-//     let card = createCard(product);
-//     productContainer.innerHTML += card;
-//   }
-// }
-
-// renderCards();
-
-let cart = [];
-
-// let item_container = document.querySelector(".products");
-
-// fetch("https://frozen-ocean-71631.herokuapp.com/get-cart/").then((request) => {
-//   request.json().then((obj) => {
-//     // console.log(obj);
-//     data = obj.data;
-//     // console.log(data);
-//     item_container.innerHTML = ``;
-//     let index = 0;
-//     data.forEach((product) => {
-//       item_container.innerHTML += `<div class="item">
-//          <img src="https://picsum.photos/200/200?random=${product[0]}" alt="${product[1]}">
-//          <p class="product-name">Name: ${product[1]}</p>
-//          <p class="product-price">Price: ${product[2]}</p>
-//          <p class="product-date">Date: ${product[3]}</p>
-//          <button onclick="addToCart(${product[0]})" class="btn-Add btn-Add-${product[0]}">Add to cart</button>
-//      </div>`;
-//       //   console.log(index);
-//       index++;
-//     });
-//   });
-// });
-
-// function addToCart(index) {
-//   cart.push(index);
-//   //   console.log(cart);
-//   let add_btn = document.querySelector(`.btn-Add-${index}`);
-//   //   console.log(add_btn);
-//   add_btn.style.display = "none";
-//   populateCart();
-// }
-
-// function showCart() {
-//   document.querySelector(".cart").classList.toggle("active");
-// }
-
 function populateCart() {
   fetch("https://frozen-ocean-71631.herokuapp.com/get-cart/").then(
     (request) => {
@@ -106,12 +13,12 @@ function populateCart() {
           data.forEach((product) => {
             if (product[0] == order) {
               // console.log(product);
-              total_cost += parseFloat(product[2]);
+              total_cost += parseFloat(product[3].replace("R", ""));
               cart_container.innerHTML += `<div class="cart-item">
-            <p class="id">${product[0]}</p>
-            <p class="name">${product[1]}</p>
-            <p class="price">${product[2]}</p>
-            <p class="quantity">1</p>
+            <p class="id">Title: ${product[1]}</p>
+            <p class="name">Description: ${product[2]}</p>
+            <p class="price">Price: ${product[3]}</p>
+            <p class="quantity">Quantity: 1</p>
           </div>`;
             }
           });
@@ -125,6 +32,6 @@ function populateCart() {
 function createTotal(cost) {
   let total_container = document.querySelector(".cart");
   total_container.innerHTML += `<div class="total-div">
-  <p class="total-p">Total : ${cost}</p>
+  <p class="total-p">Total : R${cost}</p>
   </div>`;
 }
--- js/products.js
+++ js/products.js
@@ -1,188 +1,15 @@
-// let products = [
-//   {
-//     class: "card1",
-//     title: "Hugo Boss",
-//     description: "Makes stink stink go bye bye.",
-//     techStack: "Personal Hygiene",
-//     price: "R694.20",
-//     image: "./images/cologne.jpg",
-//     image_alt: "Cologne",
-//   },
-//   {
-//     class: "card2",
-//     title: "Taco shells",
-//     description: "Get these taco shells, but a little dry without the mix.",
-//     techStack: "Food",
-//     price: "R54.20",
-//     image: "./images/tacos.jpeg",
-//     image_alt: "Taco Shells",
-//   },
-//   {
-//     class: "card3",
-//     title: "Milk",
-//     description: "Coffee is not the same without this guy.",
-//     techStack: "Food",
-//     price: "R16.90",
-//     image: "./images/milk.jpeg",
-//     image_alt: "Milk",
-//   },
-//   {
-//     class: "card4",
-//     title: "Hand sanitiser",
-//     description: "The covivo is real, keep those hands clean.",
-//     techStack: "Personal Hygiene",
-//     price: "R42.00",
-//     image: "./images/Sanitiser.png",
-//     image_alt: "Hand sanitiser",
-//   },
-//   {
-//     class: "card5",
-//     title: "Headphones",
-//     description: "Someone annoying talking? Wont even need to plug this in.",
-//     techStack: "Electronics",
-//     price: "R420.69",
-//     image: "./images/headphones2.jpg",
-//     image_alt: "Heaphones",
-//   },
-//   {
-//     class: "card6",
-//     title: "Portable Lamps",
-//     description: "Eskom will get you, atleast be prepared.",
-//     techStack: "Electronics",
-//     price: "R242.69",
-//     image: "./images/lamp2.jpg",
-//     image_alt: "Portable Lamp",
-//   },
-//   {
-//     class: "card1",
-//     title: "Hugo Boss",
-//     description: "Makes stink stink go bye bye.",
-//     techStack: "Personal Hygiene",
-//     price: "R694.20",
-//     image: "./images/cologne.jpg",
-//     image_alt: "Cologne",
-//   },
-//   {
-//     class: "card2",
-//     title: "Taco shells",
-//     description: "Get these taco shells, but a little dry without the mix.",
-//     techStack: "Food",
-//     price: "R54.20",
-//     image: "./images/tacos.jpeg",
-//     image_alt: "Taco Shells",
-//   },
-//   {
-//     class: "card3",
-//     title: "Milk",
-//     description: "Coffee is not the same without this guy.",
-//     techStack: "Food",
-//     price: "R16.90",
-//     image: "./images/milk.jpeg",
-//     image_alt: "Milk",
-//   },
-//   {
-//     class: "card4",
-//     title: "Hand sanitiser",
-//     description: "The covivo is real, keep those hands clean.",
-//     techStack: "Personal Hygiene",
-//     price: "R42.00",
-//     image: "./images/Sanitiser.png",
-//     image_alt: "Hand sanitiser",
-//   },
-//   {
-//     class: "card5",
-//     title: "Headphones",
-//     description: "Someone annoying talking? Wont even need to plug this in.",
-//     techStack: "Electronics",
-//     price: "R420.69",
-//     image: "./images/headphones2.jpg",
-//     image_alt: "Heaphones",
-//   },
-//   {
-//     class: "card6",
-//     title: "Portable Lamps",
-//     description: "Eskom will get you, atleast be prepared.",
-//     techStack: "Electronics",
-//     price: "R242.69",
-//     image: "./images/lamp2.jpg",
-//     image_alt: "Portable Lamp",
-//   },
-// ];
-
-// function createCard(card) {
-//   let createdCard = `<div class="card ${card.class}" techStack="${card.techStack}">
-//           <div class="card-image">
-//           <img src="${card.image}" alt="${card.image_alt}" />
-//           <h2>${card.title}</h2>
-//           <p>${card.description}</p>
-//           <h4>${card.price}</h4>
-//         </div>
-//       `;
-//   return createdCard;
-// }
-
-// function renderCards() {
-//   let productContainer = document.querySelector(".cards");
-//   for (product of products) {
-//     let card = createCard(product);
-//     productContainer.innerHTML += card;
-//   }
-// }
-
-// renderCards();
-
-// Populate container
-
-function populateEdit(index) {
-  console.log(index);
-  let edit_container = document.querySelector(".container-edit");
-  edit_container.innerHTML = `<!-- edit container! form? -->
-  <div class="old-info">
-    <!-- old info -->
-  </div>
-  <form action="http://127.0.0.1:5000/edit-product/1/" method="PUT">
-    <!-- product_name -->
-    <div class="product-div">
-      <label class="product-info" for="product_name"
-        >The Product Name:
-      </label>
-      <input
-        class="product-input"
-        type="text"
-        name="product_name"
-        required
-        placeholder="The Product Name"
-      />
-    </div>
-
-    <!-- product_price -->
-    <div class="product-div">
-      <label class="product-info" for="product_price"
-        >The Product Price:
-      </label>
-      <input
-        class="product-input"
-        type="text"
-        name="product_price"
-        required
-        placeholder="The Product Price"
-      />
-    </div>
-
-    <!-- buttons -->
-    <button onclick="editProduct(${index})" type="button">awe</button>
-  </form>
-  <button onclick="showEdit(-1)" class="btn-edit">close</button>`;
-}
+let cart = [];
 
 // Add to Cart Function
 
 function addToCart(index) {
+  console.log(index);
   cart.push(index);
   //   console.log(cart);
   let add_btn = document.querySelector(`.btn-Add-${index}`);
   //   console.log(add_btn);
   add_btn.style.display = "none";
+  alert("You're quite lucky, This product is temporarily Out Of Stock");
   populateCart();
 }
 
@@ -210,7 +37,7 @@ function editProduct(id) {
     };
     console.log(json_dict);
     // location.href = "";
-    fetch(`http://127.0.0.1:5000/edit-product/${id}/`, {
+    fetch(`https://frozen-ocean-71631.herokuapp.com/edit-product/${index}`, {
       method: "PUT",
       body: JSON.stringify(json_dict),
     })
@@ -226,23 +53,6 @@ function editProduct(id) {
   }
 }
 
-// const data = { username: "example" };
-
-// fetch("https://example.com/profile", {
-//   method: "POST", // or 'PUT'
-//   headers: {
-//     "Content-Type": "application/json",
-//   },
-//   body: JSON.stringify(data),
-// })
-//   .then((response) => response.json())
-//   .then((data) => {
-//     console.log("Success:", data);
-//   })
-//   .catch((error) => {
-//     console.error("Error:", error);
-//   });
-
 // Delete function
 
 function deleteProduct(index) {
@@ -250,9 +60,9 @@ function deleteProduct(index) {
   let delConfirm = confirm("Are you sure you want to delete this product?");
   if (delConfirm) {
     fetch(`https://frozen-ocean-71631.herokuapp.com/remove-product/${index}`);
-    // createCards();
+    createCards();
   }
-  // createCards();
+  createCards();
 }
 
 // Show Edit Section function
@@ -302,9 +112,9 @@ function createCards() {
          <h2> ${product[1]}</h2>
          <p>${product[2]}</p>
          <h4>${product[3]}</h4>
-         <button onclick="addToCart(${product[0]})" class="btn-Add btn-Add-${product[0]}">Add to cart</button>
-         <button onclick="showEdit(${index})" class="btn-edit">Edit</button>
-         <button onclick="deleteProduct(${product[0]})" class="btn-delete">Delete</button>
+         <button onclick="addToCart(${product[0]})" class="btn-Add btn-Add-${product[0]}" id="btns">Add to cart</button>
+         <button onclick="showEdit(${index})" class="btn-edit" id="btns">Edit</button>
+         <button onclick="deleteProduct(${product[0]})" class="btn-delete" id="btns">Delete</button>
      </div>`;
           console.log(index);
           index++;
